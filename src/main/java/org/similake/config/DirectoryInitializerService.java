802467d41208c689d7c5151dde0d261f805e5bd7
Pritesh <tinumistry@gmail.com>
2024-10-27 00:36:34 +0530

    cleaning dir at startup

--- src/main/java/org/similake/config/DirectoryInitializerService.java
+++ src/main/java/org/similake/config/DirectoryInitializerService.java
@@ -8,24 +8,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class DirectoryInitializerService {
 
     private static final Logger logger = LoggerFactory.getLogger(DirectoryInitializerService.class);
 
-
     @Value("${db.path}")
-    private  String COLLECTIONS_DIR;
+    private String COLLECTIONS_DIR;
 
     @Value("${config.path}")
-    private  String CONFIG_DIR;
+    private String CONFIG_DIR;
 
-   @PostConstruct
+    @PostConstruct
     public void initDirectories() {
+        cleanAllDirectories();
         createDirectoryIfNotExists(COLLECTIONS_DIR);
         createDirectoryIfNotExists(CONFIG_DIR);
     }
@@ -41,6 +41,43 @@ public class DirectoryInitializerService {
             }
         } else {
             logger.info("Directory already exists: {}", dirPath);
+        }
+    }
+
+    public void cleanAllDirectories() {
+        cleanDirectory(COLLECTIONS_DIR);
+        cleanDirectory(CONFIG_DIR);
+    }
+
+    public void cleanDirectory(String dirPath) {
+        Path path = Paths.get(dirPath);
+        if (Files.exists(path)) {
+            try {
+                AtomicInteger deletedCount = new AtomicInteger(0);
+                Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file);
+                        deletedCount.incrementAndGet();
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        if (!dir.equals(path)) {  // Don't delete the root directory
+                            Files.delete(dir);
+                            deletedCount.incrementAndGet();
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+                logger.info("Cleaned directory: {}, {} items deleted", dirPath, deletedCount.get());
+            } catch (IOException e) {
+                logger.error("Error cleaning directory: " + dirPath, e);
+                throw new RuntimeException("Failed to clean directory: " + dirPath, e);
+            }
+        } else {
+            logger.warn("Directory does not exist: {}", dirPath);
         }
     }
 }
